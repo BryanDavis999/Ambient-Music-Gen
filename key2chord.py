0cedc70f3a014ba87e3b004ab5dd9476056c28e9
Tangerine98 <menonnandini98@gmail.com>
2019-04-29 11:57:23 +0530

    Modify keys to chords

--- key2chord.py
+++ key2chord.py
@@ -4,9 +4,10 @@ from mido import MidiFile
 import os
 import math
 
-mid = MidiFile('Trio_Samples/samples/hierdec-trio_16bar_sample_2019-04-19_115005-001-of-002.mid')
+mid = MidiFile('Piano_Samples/samples/hierdec-mel_16bar_sample_2019-04-17_234251-000-of-002.mid')
 everyNote = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B']
-print(type(everyNote))
+
+f = open("notes.txt","w")
 for j in mid.tracks:
     notes = []
     for i in j:
@@ -14,22 +15,9 @@ for j in mid.tracks:
             notes.append(i.note)
         except:
             print("No note attribute")
-    print(notes)
 
     notes_in_readable_form = []
     for i in  notes:
-        notes_in_readable_form.append(everyNote[i%12]+str(math.floor(i/12)))
-    print(notes_in_readable_form)
-
-def getFiles(location):
-    return [i for i in ( os.listdir(location) if location else os.listdir() ) if i != '.DS_Store']
-
-
-
-
-#filter_tempo('Trio_Samples/samples')
-#filter_tempo('Trio_Samples/samples2')
-
-#filter_tempo('Samples2')
-
-#rand_sample_10("Samples2")
+        notes_in_readable_form = "{}{} ".format(everyNote[i%12],str(math.floor(i/12)))
+        f.write(notes_in_readable_form)
+f.close()
