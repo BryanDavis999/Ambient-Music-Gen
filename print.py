5322cb0f4d4d4bc6fe86a847a39cd9fa87cad150
Tangerine98 <menonnandini98@gmail.com>
2019-04-29 16:31:32 +0530

    Add print offline

--- print.py
+++ print.py
@@ -5,17 +5,26 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 import time
 
-driver = webdriver.Safari()
+driver = webdriver.Firefox()
 driver.get("https://www.dcode.fr/music-sheet")
 elem = driver.find_element_by_id("convert_from_abc_to_sheet_notes_abc")
 print(elem)
 elem.clear()
-elem.send_keys("A5, G#5, A5, G#5, F#5, F#5, F#5, F#5, F#5, F#5, F#5, F#5, B5, B5, C#6, B5,")
+
+f = open ("notes.txt","r")
+notes = f.read()
+f.close()
+print(notes)
+elem.send_keys(notes)
+# time.sleep(10)
 
 button = driver.find_element_by_xpath('/html/body/div[2]/div[2]/form[4]/button')
 button.location_once_scrolled_into_view
 button.click()
 time.sleep(5)
+button.location_once_scrolled_into_view
+button.click()
+time.sleep(10)
 
 driver.find_element_by_xpath("/html/body/div[2]/div[1]/div/div[4]/span[2]/form/button[2]").click()
 
